fa67f58dad988e02e27166db13c376d9854609a1
Udofia, Godwin <46636890+aifodu@users.noreply.github.com>
2024-08-14 18:19:47 +0100

    feat: add dom manipulation / api lesson

---
+++ js/dom/script.js
@@ -0,0 +1,61 @@
+const form = document.querySelector('form');
+const models = document.getElementById('models');
+
+async function findCar(e) {
+  e.preventDefault();
+
+  const make = form.manufacturer.value.trim();
+  const year = form.year.value.trim();
+
+  if (!make || !year) {
+    alert('Please enter a manufacturer and a year');
+    return;
+  }
+
+  // Sample: https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformakeyear/make/honda/modelyear/2015?format=json
+
+  // const url =
+  //   "https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformakeyear/make/" +
+  //   make + "/modelyear/" + year + "?format=json";
+
+  const url = `https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformakeyear/make/${make}/modelyear/${year}?format=json`;
+
+  try {
+    models.innerHTML = '<tr><td colspan="2>Searching...</td></tr>';
+
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const jsonResponse = await response.json();
+
+    if (jsonResponse.Results.length === 0) {
+      alert('No cars found');
+      return;
+    }
+
+    let newHtml = '';
+
+    for (let i = 0; i < jsonResponse.Results.length; i++) {
+      const car = jsonResponse.Results[i];
+
+      newHtml += `
+        <tr>
+          <td>${car.Make_Name}</td>
+          <td>${car.Model_Name}</td>
+        </tr>
+      `;
+    }
+
+    models.innerHTML = newHtml;
+  } catch (error) {
+    console.error(error.message);
+  }
+
+  console.log(url);
+}
+
+
+
+form.addEventListener('submit', findCar);
